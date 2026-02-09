55897056f0087f85a30662802c1e69f535eeb537
esekiels <esekielmhs@gmail.com>
2026-02-09 13:34:36 +0700

    chore: setup project modular

---
+++ Core/Common/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "Common",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "Common",
+            targets: ["Common"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Common"
+        ),
+
+    ]
+)
