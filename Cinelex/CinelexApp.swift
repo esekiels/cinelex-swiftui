57c7b07ed92c0feb7ca3828882c47d00b527a9e3
esekiels <esekielmhs@gmail.com>
2026-02-09 13:10:06 +0700

    chore: init project

---
+++ Cinelex/CinelexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinelexApp.swift
+//  Cinelex
+//
+//  Created by Esekiel Surbakti on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CinelexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinelexTests/CinelexTests.swift
@@ -0,0 +1,17 @@
+//
+//  CinelexTests.swift
+//  CinelexTests
+//
+//  Created by Esekiel Surbakti on 09/02/26.
+//
+
+import Testing
+@testable import Cinelex
+
+struct CinelexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
